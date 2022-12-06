4db5e81269c6c2bab272afbc2b92cb3655469e8c
Kirill Skladanov <wra1th@inbox.ru>
2022-12-06 16:24:57 +0300

    <test><добавить тесты>

--- src/Main.java
+++ src/Main.java
@@ -2,9 +2,37 @@ public class Main {
 
     public static void main(String[] args) {
 
-     Task task = new Task("test", "cool", "new");
-     System.out.println(task.getId());
-     Subtask subtask = new Subtask("subtest", "cooler", "new", 2);
-        System.out.println(subtask.getId());
+        /* просто тесты
+        Task task = new Task("Первая задача", "ОБЫЧНАЯ", "NEW");
+        Manager manager = new Manager();
+
+        manager.createTask(task);
+        System.out.println(manager.getAllTasks().get(0).getStatus());
+
+        Task task1 = new Task("Вторая задача", "интересная", "NEW");
+
+        manager.createTask(task1);
+        System.out.println(manager.getAllTasks().get(1).getId());
+
+        Epic epic = new Epic("Первый эпик", "обычный", "NEW");
+
+        manager.createEpic(epic);
+        System.out.println(manager.getEpicById(epic.getId()).description);
+
+        Subtask subtask = new Subtask("Первая подзадача", "Необычная", "NEW", epic.getId());
+
+        manager.createSubtask(subtask);
+        System.out.println(manager.getSubtasksOfEpic(epic).get(0).getId());
+
+        Subtask subtask1 = new Subtask("Вторая подзадача", "Скучная", "IN_PROGRESS", epic.getId());
+
+        manager.createSubtask(subtask1);
+
+        Subtask subtaskUp = new Subtask("Первая подзадача", "Необычная", "DONE",
+                epic.getId());
+
+        manager.updateSubtask(subtaskUp);
+        System.out.println(manager.getEpicById(epic.getId()).getStatus());
+*/
     }
 }
