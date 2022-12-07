5417273274ef3c939e9f8f4ed95b2e2a9cf8397e
Kirill Skladanov <wra1th@inbox.ru>
2022-12-07 17:17:00 +0300

    <style> убрать лишние строки

--- src/tasks/Subtask.java
+++ src/tasks/Subtask.java
@@ -3,9 +3,6 @@ package tasks;
 import manager.Manager;
 
 public class Subtask extends Task {
-
-
-
     private int idEpic;
     public Subtask(String title, String description, String status, int idEpic, Manager manager) {
         super(title, description, status, manager);
