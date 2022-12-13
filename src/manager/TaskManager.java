2096791d035c6b28048a3a81779904c1ff4f842e
Kirill Skladanov <wra1th@inbox.ru>
2022-12-13 14:36:05 +0300

    <refactor> создать интерфейс

---
+++ src/manager/TaskManager.java
@@ -0,0 +1,40 @@
+package manager;
+
+import tasks.Epic;
+import tasks.Subtask;
+import tasks.Task;
+
+import java.util.ArrayList;
+
+public interface TaskManager {
+        public int getId();
+        //методы для получения списка задач
+        public ArrayList<Task> getAllTasks();
+        public ArrayList<Epic> getAllEpics();
+        public ArrayList<Subtask> getAllSubtasks();
+        // методы для удаления
+        public void deleteAllTasks();
+        public void deleteAllSubtasks();
+        public void deleteAllEpics();
+        //методы для получения по айди
+        public Task getTaskById(int id);
+        public Epic getEpicById(int id);
+        public Subtask getSubtaskById(int id);
+        // методы для удаления по айди
+        public void removeTaskById(int id);
+        public void removeEpicById(int id);
+        public void removeSubtaskById(int id);
+        //методы по созданию задач
+        public void createTask(Task task);
+        public void createEpic(Epic epic);
+        public void createSubtask(Subtask subtask);
+        //методы по обновлению задач
+        public void updateTask(Task task);
+        public void updateEpic(Epic epic);
+        public void updateSubtask(Subtask subtask);
+        public void changeEpicStatus(Subtask subtask);
+        //получение подзадач
+        public ArrayList<Subtask> getSubtasksOfEpic(Epic epic);
+    }
+
+
