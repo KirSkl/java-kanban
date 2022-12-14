f95ad83f189e798ddc490d0131c52ae385da605f
Kirill Skladanov <wra1th@inbox.ru>
2022-12-14 13:45:01 +0300

    <test> изменить тесты

--- src/Main.java
+++ src/Main.java
@@ -1,43 +1,61 @@
+import manager.*;
+import tasks.Task;
+import static manager.StatusOfTask.*;
+
+
 public class Main {
 
     public static void main(String[] args) {
-        //Хочу сказать большое спасибо за твои ревью! Нравится, что все замечания по делу, при этом ты обращаешь
-        //внимание и на какие-то крупные огрехи, и на некоторые мелочи (то есть внимательно читаешь мой код, мне бы
-        //так его читать))
-        // И круто, что ты объясняешь, почему стоит делать так или иначе, даешь советы на будущее -
-        // это очень ценно для меня!
 
-        /* просто тесты
-        tasks.Task task = new tasks.Task("Первая задача", "ОБЫЧНАЯ", "NEW");
-        manager.Manager manager = new manager.Manager();
+        //просто тесты
+        /*InMemoryTaskManager manager = new InMemoryTaskManager();
 
+        tasks.Task task = new tasks.Task("Первая задача", "ОБЫЧНАЯ", NEW, manager);
         manager.createTask(task);
-        System.out.println(manager.getAllTasks().get(0).getStatus());
-
-        tasks.Task task1 = new tasks.Task("Вторая задача", "интересная", "NEW");
-
+        tasks.Task task1 = new tasks.Task("Вторая задача", "интересная", NEW, manager);
         manager.createTask(task1);
-        System.out.println(manager.getAllTasks().get(1).getId());
-
-        tasks.Epic epic = new tasks.Epic("Первый эпик", "обычный", "NEW");
-
+        tasks.Epic epic = new tasks.Epic("Первый эпик", "обычный", NEW, manager);
         manager.createEpic(epic);
-        System.out.println(manager.getEpicById(epic.getId()).description);
-
-        tasks.Subtask subtask = new tasks.Subtask("Первая подзадача", "Необычная", "NEW", epic.getId());
+        tasks.Subtask subtask = new tasks.Subtask("Первая подзадача", "Необычная", NEW,
+                epic.getId(), manager);
 
         manager.createSubtask(subtask);
-        System.out.println(manager.getSubtasksOfEpic(epic).get(0).getId());
-
-        tasks.Subtask subtask1 = new tasks.Subtask("Вторая подзадача", "Скучная", "IN_PROGRESS", epic.getId());
+        tasks.Subtask subtask1 = new tasks.Subtask("Вторая подзадача", "Скучная",
+                IN_PROGRESS, epic.getId(), manager);
 
         manager.createSubtask(subtask1);
-
-        tasks.Subtask subtaskUp = new tasks.Subtask("Первая подзадача", "Необычная", "DONE",
-                epic.getId());
-
-        manager.updateSubtask(subtaskUp);
+        //System.out.println(manager.getAllTasks().get(0).getStatus());
+        manager.getTaskById(0);
+        System.out.println(manager.getEpicById(epic.getId()).getDescription());
         System.out.println(manager.getEpicById(epic.getId()).getStatus());
-*/
+        System.out.println(manager.getSubtaskById(subtask1.getId()).getDescription());
+
+
+        //System.out.println(manager.getAllTasks().get(1).getId());
+
+        for (Task task2  : manager.) {
+
+            if (task2!=null) {
+                System.out.println(task2.getTitle());
+            } else System.out.println("null");
+        }
+
+
+
+
+
+        //System.out.println(manager.getSubtasksOfEpic(epic).get(0).getId());
+
+
+
+        //tasks.Subtask subtaskUp = new tasks.Subtask("Первая подзадача", "Необычная", StatusOfTask.DONE,
+                //epic.getId(), manager);
+
+        //manager.updateSubtask(subtaskUp);
+
+if (!manager.getHistory().isEmpty()) {
+            for (Task task2: manager.getHistory()) {
+
+         }*/
     }
 }
