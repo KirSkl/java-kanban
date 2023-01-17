2db0886249b39a8f548d4661bd6455d274f70dd3
Kirill Skladanov <wra1th@inbox.ru>
2023-01-17 17:20:18 +0300

    <feat> добавить и реализовать метод linkLast(), добавить мапу и класс Node

--- src/manager/InMemoryHistoryManager.java
+++ src/manager/InMemoryHistoryManager.java
@@ -2,10 +2,34 @@ package manager;
 
 import tasks.Task;
 import java.util.ArrayList;
+import java.util.HashMap;
 import java.util.List;
+import java.util.Map;
 
 public class InMemoryHistoryManager implements HistoryManager {
-    private final List<Task> historyTasks = new ArrayList<>();
+    private final Map<Integer, Node> historyTasks = new HashMap<>();
+    Node head;
+    Node tail;
+    private void linkLast(Task task) {
+        Node node = new Node(task);
+        if (historyTasks.isEmpty()) {
+            head = node;
+        } else {
+            node.prev = tail;
+        }
+        tail = node;
+        historyTasks.put(task.getId(), node);
+    }
+    class Node {
+        Task task;
+        Node prev;
+        Node next;
+        private Node(Task task) {
+            this.task = task;
+            this.prev = null;
+            this.next = null;
+        }
+    }
 
     @Override
     public void add(Task task) {
@@ -17,7 +41,7 @@ public class InMemoryHistoryManager implements HistoryManager {
 
     @Override
     public void remove(int id) {
-
+        //пока здесь возможен только цикл, пробегающий по всему списку
     }
 
     @Override
