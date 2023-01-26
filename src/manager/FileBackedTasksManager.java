60a0eeb67ba02dd01f9a1a68f58219d9b71c8e48
Kirill Skladanov <wra1th@inbox.ru>
2023-01-26 18:34:47 +0300

    <feat> добавить конструктор

--- src/manager/FileBackedTasksManager.java
+++ src/manager/FileBackedTasksManager.java
@@ -1,4 +1,10 @@
 package manager;
 
 public class FileBackedTasksManager extends InMemoryTaskManager implements TaskManager {
+    String path;
+    public FileBackedTasksManager (String path) {
+        this.path = path;
+    }
+
+    private void save(){}
 }
