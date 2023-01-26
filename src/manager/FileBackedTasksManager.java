c585ac9a1ab64f2d81769bfc10e3663aa3c7e535
Kirill Skladanov <wra1th@inbox.ru>
2023-01-26 18:47:30 +0300

    <feat> сгенерировать перезапись методов

--- src/manager/FileBackedTasksManager.java
+++ src/manager/FileBackedTasksManager.java
@@ -1,5 +1,9 @@
 package manager;
 
+import tasks.Epic;
+import tasks.Subtask;
+import tasks.Task;
+
 public class FileBackedTasksManager extends InMemoryTaskManager implements TaskManager {
     String path;
     public FileBackedTasksManager (String path) {
@@ -7,4 +11,87 @@ public class FileBackedTasksManager extends InMemoryTaskManager implements TaskM
     }
 
     private void save(){}
+
+    @Override
+    public void deleteAllTasks() {
+        super.deleteAllTasks();
+        
+    }
+
+    @Override
+    public void deleteAllSubtasks() {
+        super.deleteAllSubtasks();
+    }
+
+    @Override
+    public void deleteAllEpics() {
+        super.deleteAllEpics();
+    }
+
+    @Override
+    public Task getTaskById(int id) {
+        return super.getTaskById(id);
+    }
+
+    @Override
+    public Epic getEpicById(int id) {
+        return super.getEpicById(id);
+    }
+
+    @Override
+    public Subtask getSubtaskById(int id) {
+        return super.getSubtaskById(id);
+    }
+
+    @Override
+    public void removeTaskById(int id) {
+        super.removeTaskById(id);
+    }
+
+    @Override
+    public void removeEpicById(int id) {
+        super.removeEpicById(id);
+    }
+
+    @Override
+    public void removeSubtaskById(int id) {
+        super.removeSubtaskById(id);
+    }
+
+    @Override
+    public void createTask(Task task) {
+        super.createTask(task);
+    }
+
+    @Override
+    public void createEpic(Epic epic) {
+        super.createEpic(epic);
+    }
+
+    @Override
+    public void createSubtask(Subtask subtask) {
+        super.createSubtask(subtask);
+    }
+
+    @Override
+    public void updateTask(Task task) {
+        super.updateTask(task);
+    }
+
+    @Override
+    public void updateEpic(Epic epic) {
+        super.updateEpic(epic);
+    }
+
+    @Override
+    public void updateSubtask(Subtask subtask) {
+        super.updateSubtask(subtask);
+    }
+
+    @Override
+    public void changeEpicStatus(Subtask subtask) {
+        super.changeEpicStatus(subtask);
+    }
 }
+
+
