2826687534223bae1b4224e6d90e2dc46c2a5f27
Kirill Skladanov <wra1th@inbox.ru>
2023-03-23 01:08:56 +0300

    <feat> добавить метод Equals(). Добавить временные поля в конструктор

--- src/tasks/Subtask.java
+++ src/tasks/Subtask.java
@@ -2,17 +2,25 @@ package tasks;
 
 import manager.InMemoryTaskManager;
 
+import java.time.Instant;
+import java.util.Objects;
+
 public class Subtask extends Task {
     private int idEpic;
 
-    public Subtask(String title, String description, StatusOfTask status, int idEpic, InMemoryTaskManager manager,
-                   TypeOfTask type) {
-        super(title, description, status, manager, type);
+    public Subtask(String title, String description, StatusOfTask status, int idEpic, TypeOfTask type,
+                       Instant startTime, long duration) {
+        super(title, description, status, type, startTime, duration);
+        this.idEpic = idEpic;
+    }
+    public Subtask(int id, String title, String description, StatusOfTask status, int idEpic, TypeOfTask type,
+                       Instant startTime, long duration) {
+        super(id, title, description, status, type, startTime, duration );
         this.idEpic = idEpic;
     }
     public Subtask(String[] fields) {
         super(fields);
-        idEpic = Integer.parseInt(fields[5]);
+        idEpic = Integer.parseInt(fields[8]);
     }
     public int getIdEpic() {
         return idEpic;
@@ -20,5 +28,18 @@ public class Subtask extends Task {
     @Override
     public String toString() {
         return super.toString() + idEpic;
+    }
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) return true;
+        if (!super.equals(o)) return false;
+
+        Subtask subtask = (Subtask) o;
+
+        return Objects.equals(this.idEpic, subtask.idEpic);
+    }
+    @Override
+    public int hashCode() {
+        return Objects.hash(super.hashCode(), idEpic);
     }
 }
