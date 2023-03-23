2dab39cdea8eb641b636cbb44ab9e1dfa7fb124e
Kirill Skladanov <wra1th@inbox.ru>
2023-03-23 13:26:53 +0300

    <feat> добавить метод Equals(). Добавить временные поля

--- src/tasks/Task.java
+++ src/tasks/Task.java
@@ -1,6 +1,7 @@
 package tasks;
 
-import manager.InMemoryTaskManager;
+import java.time.Instant;
+import java.util.Objects;
 
 public class Task {
     protected String title;
@@ -8,13 +9,27 @@ public class Task {
     protected int id;
     protected StatusOfTask status;
     protected TypeOfTask type;
+    protected Instant startTime;
+    protected long duration;
 
-    public Task(String title, String description, StatusOfTask status, InMemoryTaskManager manager, TypeOfTask type) {
+    public Task(String title, String description, StatusOfTask status, TypeOfTask type, Instant startTime,
+                long duration) {
         this.title = title;
         this.description = description;
-        this.id = manager.getId();
         this.status = status;
         this.type = type;
+        this.startTime = startTime;
+        this.duration = duration;
+    }
+    public Task(int id, String title, String description, StatusOfTask status, TypeOfTask type, Instant startTime,
+                long duration) {
+        this.id = id;
+        this.title = title;
+        this.description = description;
+        this.status = status;
+        this.type = type;
+        this.startTime = startTime;
+        this.duration = duration;
     }
     public Task(String[] lines) {
         id = Integer.parseInt(lines[0]);
@@ -22,10 +37,28 @@ public class Task {
         title = lines[2];
         status = StatusOfTask.valueOf(lines[3]);
         description = lines[4];
+        startTime = Instant.parse(lines[5]);
+        duration = Long.parseLong(lines[6]);
     }
     public int getId() {
         return id;
     }
+    public void setId(int id) {
+        this.id = id;
+    }
+    public Instant getStartTime() {
+        return startTime;
+    }
+    public void setStartTime(Instant startTime) {
+        this.startTime = startTime;
+    }
+    public long getDuration() {
+        return duration;
+    }
+    public void setDuration(long duration) {
+        this.duration = duration;
+    }
+
     public void setStatus(StatusOfTask status) {
         this.status = status;
     }
@@ -39,9 +72,29 @@ public class Task {
         return title;
     }
     public String toString() {
-        return id + "," + type + "," + title + "," + status + "," + description + ",";
+        return id + "," + type + "," + title + "," + status + "," + description + "," + startTime + "," + duration
+                + "," + getEndTime() + ",";
     }
     public TypeOfTask getType() {
         return type;
+    }
+    public Instant getEndTime() {
+        final int SECONDS_IN_MINUTE = 60;
+        return startTime.plusSeconds(duration*SECONDS_IN_MINUTE);
+    }
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) return true;
+
+        Task object = (Task) o;
+
+        return Objects.equals(this.title, object.title)
+                && Objects.equals(this.description, object.description)
+                && Objects.equals(this.id, object.id)
+                && Objects.equals(this.status, object.status);
+    }
+    @Override
+    public int hashCode() {
+        return Objects.hash(title, description, id, status);
     }
 }
