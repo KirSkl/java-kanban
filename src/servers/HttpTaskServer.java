4218759ebcd0f5677e88ee8f70165589e3f81c0c
Kirill Skladanov <wra1th@inbox.ru>
2023-04-06 21:00:07 +0300

    <feat> реализовать обработку всех эндпойнтов

--- src/servers/HttpTaskServer.java
+++ src/servers/HttpTaskServer.java
@@ -2,9 +2,12 @@ package servers;
 
 import com.google.gson.Gson;
 import com.google.gson.JsonElement;
+import com.google.gson.JsonParser;
 import com.sun.net.httpserver.HttpExchange;
 import com.sun.net.httpserver.HttpServer;
 import manager.*;
+import tasks.Epic;
+import tasks.Subtask;
 import tasks.Task;
 
 import java.io.IOException;
@@ -14,6 +17,7 @@ import java.nio.charset.Charset;
 import java.nio.charset.StandardCharsets;
 import java.time.Duration;
 import java.time.Instant;
+import java.util.List;
 import java.util.regex.Pattern;
 
 import static java.nio.charset.StandardCharsets.UTF_8;
@@ -35,31 +39,38 @@ public class HttpTaskServer {
         gson = Managers.getGson();
     }
 
+    public static void main(String[] args) throws IOException {
+        HttpTaskServer httpTaskServer = new HttpTaskServer();
+        httpTaskServer.start();
+        //httpTaskServer.stop();
+        httpTaskServer.manager.createTask(
+                new tasks.Task("Первая задача", "ОБЫЧНАЯ", NEW, TASK, Instant.EPOCH, Duration.ofMinutes(1)));
+        httpTaskServer.manager.getTaskById(1);
+    }
+
     private void handleTasks(HttpExchange httpExchange) {
         try {
             String method = httpExchange.getRequestMethod();
             String path = httpExchange.getRequestURI().getPath();
             String params = httpExchange.getRequestURI().getQuery();
             switch (method) {
-                case "GET" :
+                case "GET":
                     if (Pattern.matches("^/tasks$", path)) {
-                        System.out.println("Обработка запроса на получение приоритезированного списка задач...");
-                        var priorTasks = manager.getPrioritizedTasks();
-                        sendObject(httpExchange, priorTasks);
-
+                        System.out.println("Обработка запроса на получение приоритизированного списка задач...");
+                        sendObject(httpExchange, manager.getPrioritizedTasks());
                     } else if (Pattern.matches("/tasks/hystory$", path)) {
                         System.out.println("Обработка запроса на получение истории...");
-                        manager.getHistory();
+                        sendObject(httpExchange, manager.getHistory());
                     } else if (params == null) {
                         if (Pattern.matches("^/tasks/task$", path)) {
                             System.out.println("Обработка запроса на получение списка всех задач...");
-                            manager.getAllTasks();
+                            sendObject(httpExchange, manager.getAllTasks());
                         } else if (Pattern.matches("^/tasks/epic$", path)) {
                             System.out.println("Обработка запроса на получение списка всех эпиков...");
-                            manager.getAllEpics();
+                            sendObject(httpExchange, manager.getAllEpics());
                         } else if (Pattern.matches("^/tasks/subtask$", path)) {
                             System.out.println("Обработка запроса на получение списка всех подзадач...");
-                            manager.getAllSubtasks();
+                            sendObject(httpExchange, manager.getAllSubtasks());
                         } else {
                             System.out.println("Указан неверный путь для запроса");
                             httpExchange.sendResponseHeaders(405, 0);
@@ -67,20 +78,20 @@ public class HttpTaskServer {
                     } else if (Pattern.matches("id=\\d+$", params)) {
                         String paramsId = params.replaceFirst("id=", "");
                         int id = parseId(paramsId);
-                        if (id!=-1) {
+                        if (id != -1) {
                             if (Pattern.matches("^/tasks/task/$", path)) {
                                 System.out.println("Обработка запроса получения задачи по ID=" + id + "...");
-                                manager.getTaskById(id);
+                                sendObject(httpExchange, manager.getTaskById(id));
                             } else if (Pattern.matches("^/tasks/epic/$", path)) {
                                 System.out.println("Обработка запроса получения эпика по ID=" + id + "...");
-                                manager.getEpicById(id);
+                                sendObject(httpExchange, manager.getEpicById(id));
                             } else if (Pattern.matches("^/tasks/subtask/$", path)) {
                                 System.out.println("Обработка запроса получения подзадачи по ID=" + id + "...");
-                                manager.getSubtaskById(id);
+                                sendObject(httpExchange, manager.getSubtaskById(id));
                                 return;
                             } else if (Pattern.matches("^/tasks/subtask/epic/$", path)) {
                                 System.out.println("Обработка запроса получения подзадач эпика по ID=" + id + "...");
-                                manager.getSubtasksOfEpic(manager.getEpicByIdNoHistory(id));
+                                sendObject(httpExchange, manager.getSubtasksOfEpic(manager.getEpicByIdNoHistory(id)));
                             } else {
                                 System.out.println("Указан неверный путь для запроса");
                                 httpExchange.sendResponseHeaders(405, 0);
@@ -94,37 +105,49 @@ public class HttpTaskServer {
                         httpExchange.sendResponseHeaders(405, 0);
                     }
                     break;
-                case "POST" :
-                    /*f (Pattern.matches("/tasks/task", path)) {
-                        manager.createTask();
+                case "POST":
+                    if (Pattern.matches("/tasks/task", path)) {
+                        createOrUpdateTask(httpExchange);
+                    } else if (Pattern.matches("/tasks/subtask", path)) {
+                        createOrUpdateSubtask(httpExchange);
+                    } else if (Pattern.matches("tasks/epic", path)) {
+                        createOrUpdateEpic(httpExchange);
+                    } else {
+                        System.out.println("Указан неверный путь для запроса.");
+                        httpExchange.sendResponseHeaders(405, 0);
                     }
-                    break;*/
-                case "DELETE" :
+                    break;
+                case "DELETE":
                     if (params == null) {
                         if (Pattern.matches("^/tasks/task$", path)) {
                             System.out.println("Обработка запроса на удаление списка всех задач...");
                             manager.deleteAllTasks();
+                            httpExchange.sendResponseHeaders(200, 0);
                         } else if (Pattern.matches("^/tasks/epic$", path)) {
                             System.out.println("Обработка запроса на удаление списка всех эпиков...");
                             manager.deleteAllEpics();
+                            httpExchange.sendResponseHeaders(200, 0);
                         } else if (Pattern.matches("^/tasks/subtask$", path)) {
                             System.out.println("Обработка запроса на удаление списка всех подзадач...");
                             manager.deleteAllSubtasks();
+                            httpExchange.sendResponseHeaders(200, 0);
                         }
                     } else if (Pattern.matches("id=\\d+$", params)) {
                         String paramsId = params.replaceFirst("id=", "");
                         int id = parseId(paramsId);
-                        if (id!=-1) {
+                        if (id != -1) {
                             if (Pattern.matches("^/tasks/task/$", path)) {
                                 System.out.println("Обработка запроса удаления задачи по ID=" + id + "...");
                                 manager.removeTaskById(id);
+                                httpExchange.sendResponseHeaders(200, 0);
                             } else if (Pattern.matches("^/tasks/epic/$", path)) {
                                 System.out.println("Обработка запроса удаления эпика по ID=" + id + "...");
                                 manager.removeEpicById(id);
+                                httpExchange.sendResponseHeaders(200, 0);
                             } else if (Pattern.matches("^/tasks/subtask/$", path)) {
                                 System.out.println("Обработка запроса удаления подзадачи по ID=" + id + "...");
                                 manager.removeSubtaskById(id);
-                                return;
+                                httpExchange.sendResponseHeaders(200, 0);
                             } else {
                                 System.out.println("Указан неверный путь для запроса");
                                 httpExchange.sendResponseHeaders(405, 0);
@@ -138,7 +161,7 @@ public class HttpTaskServer {
                         httpExchange.sendResponseHeaders(405, 0);
                     }
                     break;
-                default :
+                default:
                     System.out.println("Ожидается метод GET/POST/DELETE. Текущий метод - " + method);
                     httpExchange.sendResponseHeaders(405, 0);
             }
@@ -148,6 +171,7 @@ public class HttpTaskServer {
             httpExchange.close();
         }
     }
+
     private int parseId(String id) {
         try {
             return Integer.parseInt(id);
@@ -156,25 +180,17 @@ public class HttpTaskServer {
             return -1;
         }
     }
+
     public void start() {
         System.out.println("Запускаем сервер на порту " + PORT);
         System.out.println("Открой в браузере http://localhost:" + PORT + "/");
         server.start();
     }
+
     public void stop() {
         server.stop(0);
         System.out.println("HTTP-сервер остановлен на " + PORT + " порту.");
     }
-
-    public static void main(String[] args) throws IOException {
-        HttpTaskServer httpTaskServer = new HttpTaskServer();
-        httpTaskServer.start();
-        //httpTaskServer.stop();
-        httpTaskServer.manager.createTask(new tasks.Task("Первая задача", "ОБЫЧНАЯ", NEW, TASK, Instant.EPOCH,
-                Duration.ofMinutes(1)));
-        httpTaskServer.manager.getTaskById(1);
-    }
-
 
     protected String readText(HttpExchange h) throws IOException {
         return new String(h.getRequestBody().readAllBytes(), UTF_8);
@@ -186,8 +202,72 @@ public class HttpTaskServer {
         h.sendResponseHeaders(200, resp.length);
         h.getResponseBody().write(resp);
     }
+
     protected void sendObject(HttpExchange h, Object object) throws IOException {
         String jsonString = gson.toJson(object);
         sendText(h, jsonString);
+    }
+
+    protected void createOrUpdateTask(HttpExchange httpExchange) throws IOException {
+        var inputStream = httpExchange.getRequestBody();
+        String body = new String(inputStream.readAllBytes(), DEFAULT_CHARSET);
+        Task task = gson.fromJson(body, Task.class);
+        boolean isExist = false;
+        for (Task oldTask : manager.getAllTasks()) {
+            if (task.getId() == oldTask.getId()) {
+                isExist = true;
+                break;
+            }
+        }
+        if (!isExist) {
+            manager.createTask(task);
+            System.out.println("Задача создана");
+        } else {
+            manager.updateTask(task);
+            System.out.println("Задача обновлена");
+        }
+        httpExchange.sendResponseHeaders(200, 0);
+    }
+
+    protected void createOrUpdateEpic(HttpExchange httpExchange) throws IOException {
+        var inputStream = httpExchange.getRequestBody();
+        String body = new String(inputStream.readAllBytes(), DEFAULT_CHARSET);
+        Epic epic = gson.fromJson(body, Epic.class);
+        boolean isExist = false;
+        for (Task oldEpic : manager.getAllTasks()) {
+            if (epic.getId() == oldEpic.getId()) {
+                isExist = true;
+                break;
+            }
+        }
+        if (!isExist) {
+            manager.createTask(epic);
+            System.out.println("Эпик создан");
+        } else {
+            manager.updateTask(epic);
+            System.out.println("Эпик обновлен");
+        }
+        httpExchange.sendResponseHeaders(200, 0);
+    }
+
+    protected void createOrUpdateSubtask(HttpExchange httpExchange) throws IOException {
+        var inputStream = httpExchange.getRequestBody();
+        String body = new String(inputStream.readAllBytes(), DEFAULT_CHARSET);
+        Subtask subtask = gson.fromJson(body, Subtask.class);
+        boolean isExist = false;
+        for (Task oldSubtask : manager.getAllTasks()) {
+            if (subtask.getId() == oldSubtask.getId()) {
+                isExist = true;
+                break;
+            }
+        }
+        if (!isExist) {
+            manager.createTask(subtask);
+            System.out.println("Подзадача создана");
+        } else {
+            manager.updateTask(subtask);
+            System.out.println("Подзадача обновлена");
+        }
+        httpExchange.sendResponseHeaders(200, 0);
     }
 }
