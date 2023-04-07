782c88195be8aaa9343857e33a6fb05c080656f9
Kirill Skladanov <wra1th@inbox.ru>
2023-04-07 21:51:55 +0300

    <feat> изменить менеджер по умолчанию, добавит получение Gson

--- src/manager/Managers.java
+++ src/manager/Managers.java
@@ -1,16 +1,23 @@
 package manager;
 
+
+
+import com.google.gson.Gson;
+
 import java.io.File;
 
 public class Managers {
 
     public static TaskManager getDefault() {
-        return new InMemoryTaskManager();
+        return new HttpTaskManager("http://localhost:8078");
     }
     public static HistoryManager getDefaultHistory() { //здесь тоже
         return new InMemoryHistoryManager();
     }
     public static FileBackedTasksManager getDefaultFileBackedTasksManager() {
         return new FileBackedTasksManager();
+    }
+    public static Gson getGson() {
+        return new Gson();
     }
 }
