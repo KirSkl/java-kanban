57c41edff8e5e89001a3f263eda49c1bc1adf301
Kirill Skladanov <wra1th@inbox.ru>
2023-04-11 16:27:07 +0300

    <style> небольшие изменения

--- src/tests/HttpTaskManagerTest.java
+++ src/tests/HttpTaskManagerTest.java
@@ -1,11 +1,11 @@
 package tests;
 
-import manager.FileBackedTasksManager;
 import manager.HttpTaskManager;
 import manager.Managers;
-import manager.TaskManager;
+
 import org.junit.jupiter.api.*;
 import servers.KVServer;
+
 import tasks.Epic;
 import tasks.Subtask;
 import tasks.Task;
@@ -17,19 +17,12 @@ import java.util.List;
 
 import static java.util.Collections.EMPTY_LIST;
 import static org.junit.jupiter.api.Assertions.*;
+
 import static tasks.StatusOfTask.NEW;
 import static tasks.TypeOfTask.*;
 
 public class HttpTaskManagerTest extends TaskManagerTest<HttpTaskManager> {
-    KVServer kvServer;
-    final Task task = new tasks.Task("Первая задача", "ОБЫЧНАЯ", NEW, TASK, Instant.EPOCH,
-            Duration.ofMinutes(1));
-    final Epic epic = new tasks.Epic("Первый эпик", "обычный", NEW, EPIC, Instant.EPOCH, Duration.ZERO );
-    Subtask newSubtask(Epic epic) {
-        return new Subtask("Первая подзадача", "Необычная", NEW,
-                epic.getId(), SUBTASK, Instant.EPOCH, Duration.ofMinutes(1));
-    }
-
+    private KVServer kvServer;
     @BeforeEach
      void startServer() throws IOException {
         kvServer = new KVServer();
@@ -40,25 +33,24 @@ public class HttpTaskManagerTest extends TaskManagerTest<HttpTaskManager> {
     void closeServer() {
         kvServer.stop();
     }
-
     @Test
     void saveAndLoadTest() {
         HttpTaskManager manager = Managers.getHttpTaskManager();
         manager.save(); //пустой список задач и пустая история
         manager.load();
+
         assertEquals(EMPTY_LIST, manager.getAllTasks());
 
         var newTask = manager.createTask(task);
         var newEpic = manager.createEpic(epic);
         var subtask = manager.createSubtask(newSubtask(newEpic));
-
         manager.load(); //пустая история
+
         assertEquals(EMPTY_LIST, manager.getHistory());
         //обычная работа
         manager.getTaskById(newTask.getId());
         manager.getEpicById(newEpic.getId());
         manager.getSubtaskById(subtask.getId());
-
         manager.load();
 
         assertEquals(List.of(subtask), manager.getAllSubtasks());
@@ -72,7 +64,6 @@ public class HttpTaskManagerTest extends TaskManagerTest<HttpTaskManager> {
         //эпик без подзадач
         manager.removeSubtaskById(subtask.getId());
         newEpic.clearIds();
-
         manager.load();
 
         assertEquals(List.of(newEpic), manager.getAllEpics());
@@ -80,7 +71,6 @@ public class HttpTaskManagerTest extends TaskManagerTest<HttpTaskManager> {
         //после очистки всех задач
         manager.deleteAllEpics();
         manager.deleteAllTasks();
-
         manager.load();
 
         assertEquals(EMPTY_LIST, manager.getAllTasks());
@@ -88,6 +78,4 @@ public class HttpTaskManagerTest extends TaskManagerTest<HttpTaskManager> {
         assertEquals(EMPTY_LIST, manager.getAllSubtasks());
         assertEquals(EMPTY_LIST, manager.getHistory());
     }
-
-
 }
